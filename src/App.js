12ba5851efb3aaf273870b2b67350a28b9c62199
lazyDev-ak <keneankit01062000@gmail.com>
2024-07-27 19:01:45 +0530

    New Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Editor from "./Editor";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <h1>Custom Text Editor</h1>
+        <Editor />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Editor.js
@@ -0,0 +1,57 @@
+import React, { useRef, useState } from 'react';
+import './Editor.css';
+
+const Editor = () => {
+  const editorRef = useRef(null);
+  const [selectedText, setSelectedText] = useState('');
+
+  const formatText = (command, value = null) => {
+    console.log("dataInBox "+command,"value",value);
+    document.execCommand(command, false, value);
+  };
+
+  const handleSelectionChange = () => {
+    const selection = window.getSelection();
+    const selectedText = selection.toString();
+    setSelectedText(selectedText);
+  };
+
+  const saveContent = () => {
+    const content = editorRef.current.innerHTML;
+    localStorage.setItem('editorContent', content);
+    console.log("Text",content);
+  };
+
+  const loadContent = () => {
+    const content = localStorage.getItem('editorContent');
+    if (content) {
+      editorRef.current.innerHTML = content;
+    }
+  };
+
+  return (
+    <div style={{width:"720px"}}>
+      <div className="toolbar">
+        <button onClick={() => formatText('bold')}>Bold</button>
+        <button onClick={() => formatText('italic')}>Italic</button>
+        <button onClick={() => formatText('underline')}>Underline</button>
+        <button onClick={() => formatText('fontSize', '7')}>Large Text</button>
+        <button onClick={() => formatText('foreColor', 'red')}>Red Text</button>
+        <button onClick={() => formatText('insertUnorderedList')}>Bullet List</button>
+        <button onClick={saveContent}>Save</button>
+        <button onClick={loadContent}>Load</button>
+      </div>
+      <div
+        className="editor"
+        contentEditable
+        ref={editorRef}
+        onInput={() => console.log(editorRef.current.innerHTML)}
+        onMouseUp={handleSelectionChange}
+        onKeyUp={handleSelectionChange}
+      ></div>
+      {selectedText && <div>Selected Text: {selectedText}</div>}
+    </div>
+  );
+};
+
+export default Editor;
